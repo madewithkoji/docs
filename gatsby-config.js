bcbf360f7168c897b1e264fa36a1f79a89653da5
Sean <sean@vigorwebsolutions.com>
2020-07-06 13:40:27 -0700

    poc

---
+++ gatsby-config.js
@@ -0,0 +1,27 @@
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/gatsby-config/
+ */
+
+module.exports = {
+  /* Your site config here */
+  plugins: [
+    {
+      resolve: `gatsby-transformer-asciidoc`,
+      options: {
+        attributes: {
+          showtitle: true,
+          imagesdir: `/images`,
+        },
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `pages`,
+        path: `${__dirname}/src`,
+      },
+    },
+  ],
+}
