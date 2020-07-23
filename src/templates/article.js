9eaff443919cb48ecd3b461a50fb80f374bcf155
Sean <sean@vigorwebsolutions.com>
2020-07-23 12:02:22 -0700

    Replace default text in links with page titles; add meta title

--- src/templates/article.js
+++ src/templates/article.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
+import Helmet from 'react-helmet';
 import hljs from 'highlight.js';
 import styled from 'styled-components';
 import 'highlight.js/styles/github.css';
@@ -23,18 +24,48 @@ export const query = graphql`
         main
       }
     }
+    allAsciidoc {
+      edges {
+        node {
+          id
+          html
+          document {
+            title
+          }
+          pageAttributes {
+            slug
+          }
+        }
+      }
+    }
   }
 `;
 
 const Article = (props) => {
+  const resolveTitleFromSlug = (slug) => {
+    const match = props.data.allAsciidoc.edges.map(({ node }) => node).find(({ pageAttributes: { slug: s } = {} }) => s === slug);
+    if (match) return match.document.title;
+
+    return null;
+  };
+
   useEffect(() => {
     document.querySelectorAll('pre code').forEach((block) => {
       hljs.highlightBlock(block);
     });
-  });
+
+    document.querySelectorAll('a[data-slug]').forEach((elem) => {
+      const { slug } = elem.dataset;
+      // eslint-disable-next-line no-param-reassign
+      if (slug) elem.innerText = resolveTitleFromSlug(slug) || elem.innerText;
+    });
+  }, []);
 
   return (
     <Container maxWidth="lg">
+      <Helmet>
+        <title>{props.data.asciidoc.document.title}</title>
+      </Helmet>
       <Content
         dangerouslySetInnerHTML={{ __html: props.data.asciidoc.html }}
       />
