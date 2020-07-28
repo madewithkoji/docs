61b0901673a89972f3b6fa77d8d79cd56542d063
Sean <sean@vigorwebsolutions.com>
2020-07-28 12:14:04 -0700

    Link styles

--- src/components/MobileDrawer.js
+++ src/components/MobileDrawer.js
@@ -1,55 +1,42 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Drawer from '@material-ui/core/Drawer';
-import { makeStyles } from '@material-ui/core/styles';
 import List from '@material-ui/core/List';
 import ListItemText from '@material-ui/core/ListItemText';
 import NestedList from './NestedList';
 
-const useStyles = makeStyles({
-  paper: {
-    marginTop: 0,
-    zIndex: 0,
-  },
-});
 
-const MobileDrawer = (props) => {
-  const classes = useStyles();
-
-  return (
-    <Drawer
-      classes={{ paper: classes.paper }}
-      open={props.open}
-    >
-      <div style={{ width: '240px', paddingLeft: '16px' }}>
-        {
-          props.navItems.sort((a, b) => a.idx - b.idx).map((navItem) => (
-            <List
-              component={'nav'}
-              aria-labelledby={'nested-list-subheader'}
-              className={classes.root}
-              key={navItem.name}
-            >
-              <ListItemText primary={navItem.name} />
-              <>
-                {
-                  navItem.sections.sort((a, b) => a.idx - b.idx).map((section) => (
-                    <NestedList
-                      key={section.name}
-                      openByDefault={props.location.pathname.includes(section.root)}
-                      pathname={props.location.pathname}
-                      section={section}
-                    />
-                  ))
-                }
-              </>
-            </List>
-          ))
-        }
-      </div>
-    </Drawer>
-  );
-};
+const MobileDrawer = (props) => (
+  <Drawer
+    open={props.open}
+  >
+    <div style={{ width: '240px', paddingLeft: '16px' }}>
+      {
+        props.navItems.sort((a, b) => a.idx - b.idx).map((navItem) => (
+          <List
+            component={'nav'}
+            aria-labelledby={'nested-list-subheader'}
+            key={navItem.name}
+          >
+            <ListItemText primary={navItem.name} />
+            <>
+              {
+                navItem.sections.sort((a, b) => a.idx - b.idx).map((section) => (
+                  <NestedList
+                    key={section.name}
+                    openByDefault={props.location.pathname.includes(section.root)}
+                    pathname={props.location.pathname}
+                    section={section}
+                  />
+                ))
+              }
+            </>
+          </List>
+        ))
+      }
+    </div>
+  </Drawer>
+);
 
 MobileDrawer.propTypes = {
   location: PropTypes.object,
