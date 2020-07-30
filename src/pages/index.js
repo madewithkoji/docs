9c7e60866b50c0506d202759a9a4df8b2579be4c
Rachel Sienko <rachelsienko@MacBook-Pro.local>
2020-07-30 16:24:49 -0700

    Lots of content and nav fixes

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,30 +15,34 @@ const Button = styled.button`
 
 const IndexPage = () => (
   <Container>
-    <IFrameWrapper>
-      <iframe
-        src={'https://koji.to/4kUw'}
-        title={'Explore a Koji Template'}
-        height={'522px'}
-        width={'350px'}
-      />
-    </IFrameWrapper>
+  <br />
+  <br />
     <h1>{'Develop the future of social with remixable applications'}</h1>
     <p>
-      {`Kojis are mini web applications that can be shared on all social platforms, 
-        embedded in any website, and sent via messengers and email. Everyday users can 
+      {`Kojis are mini web applications that can be shared on all social platforms,
+        embedded in any website, and sent via messengers and email. Everyday users can
         create Kojis in minutes by “remixing” templates and customizing them.`}
       <br />
       <br />
-      {`As a developer, you can build the templates that power Kojis, and your 
-        templates will be remixed into custom web applications and shared by millions 
+      {`As a developer, you can build the templates that power Kojis, and your
+        templates will be remixed into custom web applications and shared by millions
         of people across the world.`}
     </p>
-    <Button>
       <Link to={'/docs/blueprints/magazine-cover'}>
         {'Build your first template'}
       </Link>
-    </Button>
+      <br />
+      <hr />
+    <h1>{'Kojis are JavaScript applications'}</h1>
+    <p>
+      {`Kojis are full-stack, modern JavaScript applications that take advantage of the latest web standards and emerging technologies. They are designed to be responsive, so that they can run on any browser and every device. They can embed multimedia, leverage web AR/VR support, facilitate e-commerce transactions, and more.`}
+      </p>
+      <br />
+      <hr />
+    <h1>{'Templates empower everyone to make amazing Kojis'}</h1>
+    <p>
+      {`Developers build “remixable” templates for any type of web application, such as interactive selfies, memes, games, and utilities. Non-developers can edit template elements without coding, such as images, text, and sounds, and create a custom version that they can share anywhere on the web. With the ability to create and share remixable, interactive content, the opportunities for creative new applications and experiences are endless.`}
+        </p>
   </Container>
 );
 
