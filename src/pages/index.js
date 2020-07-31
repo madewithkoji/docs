fd1056a9dfb4dccfbb12a64d057fd2a278429f68
Rachel Sienko <rachelsienko@MacBook-Pro.local>
2020-07-30 18:02:57 -0700

    Basic styling fixes

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,12 +13,16 @@ const Button = styled.button`
   padding: 5px;
 `;
 
+const myP = {
+    fontSize:18
+  };
+
 const IndexPage = () => (
   <Container>
   <br />
   <br />
     <h1>{'Develop the future of social with remixable applications'}</h1>
-    <p>
+    <p style={ myP }>
       {`Kojis are mini web applications that can be shared on all social platforms,
         embedded in any website, and sent via messengers and email. Everyday users can
         create Kojis in minutes by “remixing” templates and customizing them.`}
@@ -28,19 +32,21 @@ const IndexPage = () => (
         templates will be remixed into custom web applications and shared by millions
         of people across the world.`}
     </p>
+    <p style={ myP }>
       <Link to={'/docs/blueprints/magazine-cover-blueprint'}>
         {'Build your first template'}
       </Link>
+      </p>
       <br />
       <hr />
     <h1>{'Kojis are JavaScript applications'}</h1>
-    <p>
+    <p style={ myP }>
       {`Kojis are full-stack, modern JavaScript applications that take advantage of the latest web standards and emerging technologies. They are designed to be responsive, so that they can run on any browser and every device. They can embed multimedia, leverage web AR/VR support, facilitate e-commerce transactions, and more.`}
       </p>
       <br />
       <hr />
     <h1>{'Templates empower everyone to make amazing Kojis'}</h1>
-    <p>
+    <p style={ myP }>
       {`Developers build “remixable” templates for any type of web application, such as interactive selfies, memes, games, and utilities. Non-developers can edit template elements without coding, such as images, text, and sounds, and create a custom version that they can share anywhere on the web. With the ability to create and share remixable, interactive content, the opportunities for creative new applications and experiences are endless.`}
         </p>
   </Container>
--- src/templates/Asciidoc/components/Content.js
+++ src/templates/Asciidoc/components/Content.js
@@ -11,34 +11,55 @@ const Content = styled.div`
     text-decoration: underline;
   }
 
-  p {
-    font-size: 1rem;
+  p, body {
+    font-size: 16px;
   }
 
-  h1 { 
+  code {
+    font-family: Menlo, Monaco, "Liberation Mono", Consolas, monospace;
+    font-weight: normal;
+    font-size: 14px;
+    padding: 3px 4px;
+    color: #3b3b3b;
+    background-color: rgb(248, 248, 248);
+    border: 1px solid #d4d9d9
+  }
+
+  pre, pre > code {
+    line-height: 1.6;
+  }
+
+  .admonitionblock > table { border: 0; background: none; width: 100%; }
+  .admonitionblock > table td.icon { text-align: center; width: 80px; }
+  .admonitionblock > table td.icon img { max-width: none; }
+  .admonitionblock > table td.icon .title { font-weight: bold; text-transform: uppercase; }
+  .admonitionblock > table td.content { padding-left: 1.125em; padding-right: 1.25em; border-left: 1px solid #dddddd; color: #404040; }
+  .admonitionblock > table td.content > :last-child > :last-child { margin-bottom: 0; }
+
+  h1 {
     font-size: 2.125em;
   }
 
-  h2 { 
+  h2 {
     font-size: 1.6875em;
   }
 
-  h3 { 
+  h3 {
     font-size: 1.375em;
   }
 
-  h4 { 
+  h4 {
     font-size: 1.125em;
   }
 
-  h5 { 
+  h5 {
     font-size: 1.125em;
   }
 
-  h6 { 
+  h6 {
     font-size: 1em;
   }
-  
+
   img {
     max-width: 100%;
   }
