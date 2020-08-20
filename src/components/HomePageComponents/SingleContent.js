b90eb3059e7deac320376c58750dd4ff192e4d24
ASCRA <ristic.stefan@outlook.com>
2020-08-21 00:10:30 +0200

    -Minor change, h2->h1

--- src/components/HomePageComponents/SingleContent.js
+++ src/components/HomePageComponents/SingleContent.js
@@ -6,7 +6,7 @@ const SingleContent = () => {
     return(
         <PageSection backColor="#111111">
         <SectionComponent orientation="column" column alignItems="left" textColor="white">
-              <h2>{'Kojis are JavaScript applications'}</h2>
+              <h1>{'Kojis are JavaScript applications'}</h1>
                 <p>
                   {`Kojis are full-stack, modern JavaScript applications 
                   that take advantage of the latest web standards and emerging technologies. 
@@ -14,7 +14,7 @@ const SingleContent = () => {
                   They can embed multimedia, leverage web AR/VR support, facilitate e-commerce transactions, and more.`}
                 </p>
                 <br/>
-                <h2 style={{marginTop: 0}}>{'Templates empower everyone to make amazing Kojis'}</h2>
+                <h1 style={{marginTop: 0}}>{'Templates empower everyone to make amazing Kojis'}</h1>
                 <p>
                   {`Developers build “remixable” templates for any type of web application, 
                   such as interactive selfies, memes, games, and utilities. 
