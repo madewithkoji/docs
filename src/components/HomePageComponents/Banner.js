4612c03de3b2e0fc27250f5f318d8e337c5694d6
ASCRA <ristic.stefan@outlook.com>
2020-08-21 14:32:29 +0200

    -Added a function to handle window resizing

--- src/components/HomePageComponents/Banner.js
+++ src/components/HomePageComponents/Banner.js
@@ -37,8 +37,35 @@ const StyledFrame = styled.iframe`
     }
 `;
 
+function useWidnowDimensions() {
+  const hasWindow = typeof window !== 'undefined';
+
+  function getWindowDimensions() {
+    const width = hasWindow ? window.innerWidth : null;
+    const height = hasWindow ? window.innerHeight : null;
+    return{
+      width,
+      height
+    };
+  }
+
+  const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions())
+
+  useEffect(() => {
+    if(hasWindow) {
+      function handleResize() {
+        setWindowDimensions(getWindowDimensions())
+      }
+
+      window.addEventListener('resize', handleResize);
+      return () => window.removeEventListener('resize', handleResize);
+    }
+  }, [hasWindow])
+  return windowDimensions;
+}
+
 const Banner = () => {
-  const width = window.innerWidth;
+    const {width}  = useWidnowDimensions();
 
     return(
         <PageSection padding="5%">
