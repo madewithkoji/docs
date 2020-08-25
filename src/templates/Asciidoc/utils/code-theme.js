0d6c123f7c8c3f7bb75e8a22622ccc92c8f4e8b0
James Hole <jamesc.hole@gmail.com>
2020-08-25 17:37:13 +1000

    Store code block color scheme in localStorage to persist across sessions

--- src/templates/Asciidoc/utils/code-theme.js
+++ src/templates/Asciidoc/utils/code-theme.js
@@ -4,7 +4,20 @@ const changeTheme = (e) => {
 	e.preventDefault();
 	e.stopPropagation();
 	document.body.classList.toggle('darkCode');
-}
+	if (document.body.classList.contains('darkCode')) {
+		localStorage.setItem('darkCode', 1);
+	} else {
+		localStorage.removeItem('darkCode', 1);
+	}
+};
+
+const checkLocalStorage = () => {
+	if (localStorage.getItem('darkCode')) {
+		document.body.classList.add('darkCode');
+	} else {
+		document.body.classList.remove('darkCode');
+	}
+};
 
 export const addChangeThemeButton = (block) => {
 	let button = document.createElement('button');
@@ -12,4 +25,5 @@ export const addChangeThemeButton = (block) => {
 	button.innerHTML = '<i class="fa fa-adjust"></i>';
 	block.parentNode.prepend(button);
 	button.addEventListener('click', changeTheme);
+	checkLocalStorage();
 };
