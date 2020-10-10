481b8aa0b048433b36097c014eece2ece9e1bca6
Rachel Sienko <rachelsienko@MacBook-Pro.local>
2020-10-09 17:06:31 -0700

    Fixes to magazine cover per Miles

--- src/asciidoc/docs/_includes/magazineCoverReact.js
+++ src/asciidoc/docs/_includes/magazineCoverReact.js
@@ -1,10 +1,10 @@
-import React, { useEffect, useRef, useState } from "react";
-import styled from "styled-components";
-//tag::importPackages[]
-import { FeedSdk, InstantRemixing } from "@withkoji/vcc";
-//end::importPackages[]
+import React, { useEffect, useRef, useState } from 'react';
+import styled from 'styled-components';
+// tag::importPackages[]
+import { FeedSdk, InstantRemixing } from '@withkoji/vcc';
+// end::importPackages[]
 
-//tag::styles[]
+// tag::styles[]
 const Wrapper = styled.div`
   background-color: ${({ style: { bgColor } }) => bgColor};
   height: 100vh;
@@ -20,8 +20,7 @@ const MagazineCover = styled.div`
   height: ${({ style: { height } }) => height}px;
   top: ${({ style: { top } }) => top}px;
   left: ${({ style: { left } }) => left}px;
-  background: url("${({ style: { backgroundImage } }) =>
-    backgroundImage}") no-repeat center center / cover;
+  background: url("${({ style: { backgroundImage } }) => backgroundImage}") no-repeat center center / cover;
   position: absolute;
   z-index: 0;
 `;
@@ -37,62 +36,64 @@ const H1 = styled.div`
   position: absolute;
   top: ${({ y }) => `${y}%`};
   left: ${({ x }) => `${x}%`};
-  font-family: "Roboto", sans-serif;
+  font-family: 'Roboto', sans-serif;
   color: ${({ color }) => color};
   font-size: ${({ fontSize }) => `${fontSize}px`};
   max-width: 35%;
   cursor: default;
 `;
-//end::styles[]
+// end::styles[]
 
-//tag::optimizeImage[]
-const optimizeURL = url =>
-  `${url}?fit=bounds&width=${window.innerWidth - 15}&height=${window.innerHeight - 15}&optimize=medium`;
-//end::optimizeImage[]
+// tag::optimizeImage[]
+const optimizeURL = (url) => `${url}?fit=bounds&width=${window.innerWidth - 15}&height=${window.innerHeight - 15}&optimize=medium`;
+// end::optimizeImage[]
 
-//tag::instantRemixing[]
+// tag::instantRemixing[]
 const instantRemixing = new InstantRemixing();
-//end::instantRemixing[]
-//tag::feedSdk[]
+// end::instantRemixing[]
+// tag::feedSdk[]
 const feed = new FeedSdk();
-//end::feedSdk[]
+// end::feedSdk[]
 
 const App = () => {
   // Handle remixing state
-  //tag::monitorRemix[]
+  // tag::monitorRemix[]
   const [isRemixing, setIsRemixing] = useState(instantRemixing.isRemixing);
   useEffect(() => {
-    instantRemixing.onSetRemixing(isNowRemixing => {
+    instantRemixing.onSetRemixing((isNowRemixing) => {
       setIsRemixing(isNowRemixing);
     });
   }, []);
-  //end::monitorRemix[]
+  // end::monitorRemix[]
 
-  //Handle value updates
-  //tag::updateValues[]
+  // Handle value updates
+  // tag::updateValues[]
   const [magazineOptions, setMagazineOptions] = useState(instantRemixing.get(['settings', 'magazineOptions']));
   const [textOptions, setTextOptions] = useState(instantRemixing.get(['settings', 'textOptions']));
 
   useEffect(() => {
-    instantRemixing.onValueChanged(([scope = "", key = ""], value) => {
+    instantRemixing.onValueChanged(([scope = '', key = ''], value) => {
       if (scope === 'settings' && key === 'magazineOptions') setMagazineOptions(value);
-      if (scope === "settings" && key === "textOptions") setTextOptions(value);
+      if (scope === 'settings' && key === 'textOptions') setTextOptions(value);
     });
   }, []);
-  //end::updateValues[]
+  // end::updateValues[]
 
-  //Click handlers for remixing
-  //tag::clickHandlers[]
-  const handleClick = e => {
-    if (e.target.closest(".text")) {
-      instantRemixing.onPresentControl(["settings", "textOptions"]);
-    } else {
-      instantRemixing.onPresentControl(['settings', 'magazineOptions']);
+  // Click handlers for remixing
+  // tag::clickHandlers[]
+  // Click handlers for remixing
+  const handleClick = (e) => {
+    if (isRemixing) {
+      if (e.target.closest('.editable')) {
+        instantRemixing.onPresentControl(['settings', 'textOptions']);
+      } else {
+        instantRemixing.onPresentControl(['settings', 'magazineOptions']);
+      }
     }
   };
-  //end::clickHandlers[]
+  // end::clickHandlers[]
 
-  //tag::dynamicSizing[]
+  // tag::dynamicSizing[]
   // Handle dynamic resizing
   const [size, setSize] = useState({});
 
@@ -105,59 +106,59 @@ const App = () => {
       height: magazineRef.current.height,
       width: magazineRef.current.width,
       top: magazineRef.current.offsetTop,
-      left: magazineRef.current.offsetLeft
+      left: magazineRef.current.offsetLeft,
     });
   };
 
   // Re-run the set sizing function when the window is resized
   useEffect(() => {
-    window.addEventListener("resize", runSetSize);
-    return () => window.removeEventListener("resize", runSetSize);
+    window.addEventListener('resize', runSetSize);
+    return () => window.removeEventListener('resize', runSetSize);
   }, []);
-  //end::dynamicSizing[]
+  // end::dynamicSizing[]
 
-  //tag::templateReady[]
+  // tag::templateReady[]
   useEffect(() => {
     // Wrap in the final useEffect so it's sure to run after anything else
     instantRemixing.ready();
     feed.load();
   }, []);
-  //end::templateReady[]
+  // end::templateReady[]
 
   return (
-    //tag::renderTemplate[]
+    // tag::renderTemplate[]
     <Wrapper
-        onClick={handleClick}
-        style={{
-            bgColor : magazineOptions.bgColor,
-        }}
+      onClick={handleClick}
+      style={{
+        bgColor: magazineOptions.bgColor,
+      }}
     >
-        <Magazine
-            onLoad={runSetSize}
-            ref={magazineRef}
-            src={magazineOptions.magazineName}
-        />
-        {
+      <Magazine
+        onLoad={runSetSize}
+        ref={magazineRef}
+        src={magazineOptions.magazineName}
+      />
+      {
             size.height &&
             <MagazineCover
-                style={{
-                    ...size,
-                    backgroundImage: optimizeURL(magazineOptions.coverImage)
-                }}
+              style={{
+                ...size,
+                backgroundImage: optimizeURL(magazineOptions.coverImage),
+              }}
             >
-                <H1
-                    className={isRemixing ? 'editable text' : 'text'}
-                    fontSize={size.height ? (textOptions.fontSize / 200) * size.height : 20}
-                    color={textOptions.color}
-                    x={textOptions.position.x}
-                    y={textOptions.position.y}
-                >
-                    {textOptions.title}
-                </H1>
+              <H1
+                className={isRemixing ? 'editable text' : 'text'}
+                fontSize={size.height ? (textOptions.fontSize / 200) * size.height : 20}
+                color={textOptions.color}
+                x={textOptions.position.x}
+                y={textOptions.position.y}
+              >
+                {textOptions.title}
+              </H1>
             </MagazineCover>
         }
     </Wrapper>
-    //end::renderTemplate[]
+    // end::renderTemplate[]
   );
 };
 
