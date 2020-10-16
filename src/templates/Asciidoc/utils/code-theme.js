eb9a389d0e049cb65e03d7e8c8f842a60e7128b8
Sean <sean@vigorwebsolutions.com>
2020-10-16 09:29:15 -0700

    Update to use lightMode localStorage setting

--- src/templates/Asciidoc/utils/code-theme.js
+++ src/templates/Asciidoc/utils/code-theme.js
@@ -1,30 +1,31 @@
-const TRANSITION_TIME = 600;
+// const TRANSITION_TIME = 600;
 
 const changeTheme = (e) => {
-	e.preventDefault();
-	e.stopPropagation();
-	document.body.classList.toggle('darkCode');
-	if (document.body.classList.contains('darkCode')) {
-		localStorage.setItem('darkCode', 1);
-	} else {
-		localStorage.removeItem('darkCode', 1);
-	}
+  e.preventDefault();
+  e.stopPropagation();
+  document.body.classList.toggle('darkCode');
+  if (document.body.classList.contains('darkCode')) {
+    localStorage.removeItem('lightCode', 1);
+  } else {
+    localStorage.setItem('lightCode', 1);
+  }
 };
 
 const checkLocalStorage = () => {
-	if (localStorage.getItem('darkCode')) {
-		document.body.classList.add('darkCode');
-	} else {
-		document.body.classList.remove('darkCode');
-	}
+  if (localStorage.getItem('lightCode')) {
+    document.body.classList.remove('darkCode');
+  } else {
+    document.body.classList.add('darkCode');
+  }
 };
 
+// eslint-disable-next-line import/prefer-default-export
 export const addChangeThemeButton = (block) => {
-	let button = document.createElement('button');
-	button.classList.add('theme');
-	button.dataset.tooltip = "Toggle theme";
-	button.innerHTML = '<i class="fa fa-adjust"></i>';
-	block.parentNode.prepend(button);
-	button.addEventListener('click', changeTheme);
-	checkLocalStorage();
+  const button = document.createElement('button');
+  button.classList.add('theme');
+  button.dataset.tooltip = 'Toggle theme';
+  button.innerHTML = '<i class="fa fa-adjust"></i>';
+  block.parentNode.prepend(button);
+  button.addEventListener('click', changeTheme);
+  checkLocalStorage();
 };
