6f85d527490148b906518ccad3fca53e73b92185
Sean <sean@vigorwebsolutions.com>
2021-03-10 15:18:53 -0800

    Start rendering typeAliases

--- src/templates/CorePackage/utils/typeAlias.js
+++ src/templates/CorePackage/utils/typeAlias.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { renderParameterDescription, renderParameterType, parameterIsArray } from './parameter';
 
 function parseTypeAliasType(declaration) {
   if (declaration.signatures && declaration.signatures[0]) return 'function';
@@ -10,6 +11,12 @@ function getTypeAliasDescription(declaration) {
   return declaration.comment.shortText;
 }
 
+function getTypeAliasParameters(declaration) {
+  if (!declaration.signatures || !declaration.signatures[0]) return false;
+
+  return declaration.signatures[0].parameters;
+}
+
 function renderTypeAliasFunction(declaration) {
   return '';
 }
@@ -19,24 +26,66 @@ function renderTypeAliasUnion(declaration) {
 }
 
 // eslint-disable-next-line import/prefer-default-export
-export function renderTypeAlias(typeAlias) {
+export function renderTypeAlias(typeAlias, interfaces) {
   const {
     name,
-    type: { declaration = {} },
+    type,
   } = typeAlias;
+
+  let { declaration } = type;
+  if (!declaration) declaration = {};
 
   const typeAliasType = parseTypeAliasType(declaration);
   const typeAliasDescription = getTypeAliasDescription(declaration);
+  const typeAliasParameters = getTypeAliasParameters(declaration);
 
   return (
     <div key={typeAlias.id}>
       {
-        name &&
-        <h3 id={typeAlias.name}>{typeAlias.name}</h3>
+        (name && typeAliasType === 'function') &&
+        <h3 id={typeAlias.name}>
+          {`${typeAlias.name}: `}
+          {
+            typeAliasParameters &&
+            <>
+              {`(${typeAliasParameters.map(({ name: parameterName }) => parameterName).join(', ')})`}
+            </>
+          }
+        </h3>
+      }
+      {
+        (name && typeAliasType !== 'function') &&
+        <h3 id={typeAlias.name}>
+          {typeAlias.name}
+        </h3>
       }
       {
         typeAliasDescription &&
         <p>{typeAliasDescription}</p>
+      }
+      {
+        typeAliasParameters &&
+        <div>
+          <h4>{'Parameters'}</h4>
+          <div className={'ulist'}>
+            <ul>
+              {
+                typeAliasParameters.map((parameter) => (
+                  <li key={parameter.name}>
+                    <p>
+                      <code>{parameter.name}</code>
+                      {' – '}
+                      <em>{renderParameterType(parameter)}</em>
+                      {parameterIsArray(parameter) && <span>{'[]'}</span>}
+                      {parameter.flags && parameter.flags.isOptional && <span>{' (Optional)'}</span>}
+                      {renderParameterDescription(parameter, interfaces)}
+                    </p>
+                  </li>
+                ))
+              }
+            </ul>
+          </div>
+        </div>
       }
       {
         typeAliasType === 'function' &&
