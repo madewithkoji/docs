69d1ad27fe4984b46c02f338cc93c85181ef953c
Sean <sean@vigorwebsolutions.com>
2021-03-16 10:59:02 -0700

    Handle external bracketed links

--- src/templates/CorePackage/index.js
+++ src/templates/CorePackage/index.js
@@ -376,6 +376,16 @@ const CorePackage = (props) => {
     });
   }, []);
 
+  useEffect(() => {
+    document.querySelectorAll('p').forEach((paragraph) => {
+      // eslint-disable-next-line no-param-reassign
+      paragraph.innerHTML = paragraph.innerHTML.replace(new RegExp(/\[\[.*\s\|\s.*\]\]/g), (match) => {
+        const [href, link] = match.slice(2, -2).split('|').map((t) => t.trim());
+        return `<a href="${href}" target="_blank">${link}</a>`;
+      });
+    });
+  }, []);
+
   return (
     <StyledContainer maxWidth="lg">
       <SEO title={name} description={description} />
