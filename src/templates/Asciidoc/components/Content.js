8797b894870349e74d84e6cce3c408982d133bd8
eltesoro-koji <82409235+eltesoro-koji@users.noreply.github.com>
2021-06-14 19:02:58 -0700

    DC-587 Implement list style for landing page

--- src/templates/Asciidoc/components/Content.js
+++ src/templates/Asciidoc/components/Content.js
@@ -406,6 +406,22 @@ const Content = styled.div`
   .callout8:before { content: "\\2467\\00a0"; font-size: 150%; }
   .callout9:before { content: "\\2468\\00a0"; font-size: 150%; }
 
+
+  .card {
+    box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+    transition: 0.3s;
+    background-color: #F8F8F8;
+    padding: 2px 16px;
+    margin: 16px;
+    border-radius: 10px;
+    width: 45%;
+    height: 200px;
+    float: left;
+  }
+  .clearfloat {
+    clear: both;
+  }
+
 `;
 
 export default Content;
